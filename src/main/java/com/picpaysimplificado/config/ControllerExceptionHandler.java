dd105780806ad5755ed2f96f5453014c110c2a9b
Djoseffer <tom_djosefer@hotmail.com>
2024-05-12 09:10:08 -0300

    feat: exception handling created

--- src/main/java/com/picpaysimplificado/config/ControllerExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/config/ControllerExceptionHandler.java
@@ -11,16 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException e){
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException e) {
         ExceptionDto exceptionDto = new ExceptionDto("Usuário já cadastrado", "400");
         return ResponseEntity.badRequest().body(exceptionDto);
     }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity threat404(EntityNotFoundException e){
+    public ResponseEntity threat404(EntityNotFoundException e) {
         return ResponseEntity.notFound().build();
     }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity threatGeneralException(Exception e){
+    public ResponseEntity threatGeneralException(Exception e) {
         ExceptionDto exceptionDto = new ExceptionDto(e.getMessage(), "500");
         return ResponseEntity.internalServerError().body(exceptionDto);
     }
--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -3,8 +3,6 @@ package com.picpaysimplificado.services;
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.NotificationDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +15,6 @@ public class NotificationService {
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDto notificationRequest = new NotificationDto(email, message);
-
         //ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
         //if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
         //System.out.println("Erro ao enviar notificação");
